8cf41ce6286cfc89f23c2c51aa8c43f2fb57c08e
Jerry <1032246642@qq.com>
2018-08-07 09:56:27 +0800

    auto release mysql connection

--- soci/soci_session_pool.cpp
+++ soci/soci_session_pool.cpp
@@ -7,12 +7,12 @@ using namespace soci;
 
 class soci_session_pool {
     public:
-        std::shared_ptr<connection_pool> c_pool_str;
+        std::shared_ptr<connection_pool> c_pool_ptr;
         
         soci_session_pool(size_t pool_size,const std::string& uri){
-            c_pool_str = std::make_shared<connection_pool>(pool_size); 
+            c_pool_ptr = std::make_shared<connection_pool>(pool_size); 
             for(size_t i=0 ; i < pool_size; i++){
-                session& sql = c_pool_str->at(i);
+                session& sql = c_pool_ptr->at(i);
                 sql.open(uri);
             }
         }
@@ -26,14 +26,34 @@ class soci_session_pool {
         }
 
         session& get_session(size_t& pos){
-            pos = c_pool_str->lease();
-            session& sql = c_pool_str->at(pos);
+            pos = c_pool_ptr->lease();
+            session& sql = c_pool_ptr->at(pos);
             reconnect(sql);
             return sql;
         }
 
         void release(size_t pos){
-            c_pool_str->give_back(pos);
+            c_pool_ptr->give_back(pos);
+        }
+
+        // void reconnect(std::unique_ptr<session> sql_ptr){
+        //     mysql_session_backend * mysqlBackEnd = static_cast<mysql_session_backend *>(sql_ptr->get_backend());
+        //     int i = mysql_ping(mysqlBackEnd->conn_);
+        //     if(i==1){
+        //         sql_ptr->reconnect();
+        //     }
+        // }
+
+        std::unique_ptr<session> get_session(){
+            auto sql_ptr = std::make_unique<soci::session>(*c_pool_ptr);
+            mysql_session_backend * mysqlBackEnd = static_cast<mysql_session_backend *>(sql_ptr->get_backend());
+
+            int i = mysql_ping(mysqlBackEnd->conn_);
+            if(i==1){
+                sql_ptr->reconnect();
+            }
+
+            return sql_ptr;
         }
 
 };
@@ -52,6 +72,14 @@ int main(){
         sp.release(pos);
     }
 
+    std::cout<<"new way"<<std::endl;
+
+    for(int i=0;i<10;i++){
+        auto sql = sp.get_session();
+        int num;
+        *sql << "select 1;",into(num);
+        std::cout<<i<<":"<<num<<std::endl;
+    }
+
     return 0;
 }
-
